faedd41cfd65215e6c911726f0de46ec1c61a6e4
SophiaLee <sofill.lee@gmail.com>
2020-03-18 12:57:19 +0900

    Spring IoC 컨테이너와 MyBatis 프레임워크 연동하기

---
+++ bitcamp-project-server/v51_1/src/main/java/com/eomcs/lms/servlet/LessonListServlet.java
@@ -0,0 +1,29 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Scanner;
+import org.springframework.stereotype.Component;
+import com.eomcs.lms.domain.Lesson;
+import com.eomcs.lms.service.LessonService;
+import com.eomcs.util.RequestMapping;
+
+@Component
+public class LessonListServlet implements Servlet {
+
+  LessonService lessonService;
+
+  public LessonListServlet(LessonService lessonService) {
+    this.lessonService = lessonService;
+  }
+
+  @Override
+  @RequestMapping("/lesson/list")
+  public void service(Scanner in, PrintStream out) throws Exception {
+    List<Lesson> lessons = lessonService.list();
+    for (Lesson l : lessons) {
+      out.printf("%d, %s, %s ~ %s, %d\n", l.getNo(), l.getTitle(), l.getStartDate(), l.getEndDate(),
+          l.getTotalHours());
+    }
+  }
+}
---
+++ bitcamp-project-server/v51_1/src/main/java/com/eomcs/lms/servlet/MemberListServlet.java
@@ -0,0 +1,29 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Scanner;
+import org.springframework.stereotype.Component;
+import com.eomcs.lms.domain.Member;
+import com.eomcs.lms.service.MemberService;
+import com.eomcs.util.RequestMapping;
+
+@Component
+public class MemberListServlet implements Servlet {
+
+  MemberService memberService;
+
+  public MemberListServlet(MemberService memberService) {
+    this.memberService = memberService;
+  }
+
+  @Override
+  @RequestMapping("/member/list")
+  public void service(Scanner in, PrintStream out) throws Exception {
+    List<Member> members = memberService.list();
+    for (Member m : members) {
+      out.printf("%d, %s, %s, %s, %s\n", //
+          m.getNo(), m.getName(), m.getEmail(), m.getTel(), m.getRegisteredDate());
+    }
+  }
+}
