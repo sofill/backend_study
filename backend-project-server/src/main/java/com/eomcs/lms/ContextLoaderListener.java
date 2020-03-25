60ae2577b83ad5d73416ac9606c548c6bf8a1cd6
SophiaLee <sofill.lee@gmail.com>
2020-03-25 10:46:43 +0900

    log4j

--- backend-project-server/src/main/java/com/eomcs/lms/ContextLoaderListener.java
+++ backend-project-server/src/main/java/com/eomcs/lms/ContextLoaderListener.java
@@ -2,6 +2,8 @@ package com.eomcs.lms;
 
 import java.lang.reflect.Method;
 import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
@@ -15,32 +17,34 @@ import com.eomcs.util.RequestMappingHandlerMapping;
 //
 public class ContextLoaderListener implements ApplicationContextListener {
 
+  static Logger logger = LogManager.getLogger(ContextLoaderListener.class);
+
   @Override
   public void contextInitialized(Map<String, Object> context) {
 
     try {
       // Spring IoC 컨테이너 준비
-      ApplicationContext appCtx = new AnnotationConfigApplicationContext(
-          // Spring IoC의 컨테이너 정보를 담고 있는 클래스 타입을 지정
+      ApplicationContext appCtx = new AnnotationConfigApplicationContext(//
+          // Spring IoC 컨테이너의 설정 정보를 담고 있는 클래스 타입을 지정.
           AppConfig.class);
       printBeans(appCtx);
 
       // ServerApp이 사용할 수 있게 context 맵에 담아 둔다.
       context.put("iocContainer", appCtx);
 
-      System.out.println("-------------------------------------");
+      logger.debug("----------------------------");
 
       // @Component 애노테이션이 붙은 객체를 찾는다.
-      RequestMappingHandlerMapping handlerMapper =
+      RequestMappingHandlerMapping handlerMapper = //
           new RequestMappingHandlerMapping();
       String[] beanNames = appCtx.getBeanNamesForAnnotation(Component.class);
       for (String beanName : beanNames) {
         Object component = appCtx.getBean(beanName);
 
-        // @RequestHandler 가 붙은 메서드를 찾는다.
+        // @RequestHandler가 붙은 메서드를 찾는다.
         Method method = getRequestHandler(component.getClass());
         if (method != null) {
-          // 클라이언트 명령으르 처리하는 메서드 정보를 준비한다.
+          // 클라이언트 명령을 처리하는 메서드 정보를 준비한다.
           RequestHandler requestHandler = new RequestHandler(method, component);
 
           // 명령을 처리할 메서드를 찾을 수 있도록
@@ -49,21 +53,23 @@ public class ContextLoaderListener implements ApplicationContextListener {
         }
       }
 
-      // ServerApp 에서 Request handler 를 사용할 수 있도록 공유한다.
+      // ServerApp 에서 request handler를 사용할 수 있도록 공유한다.
       context.put("handlerMapper", handlerMapper);
+
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
   private void printBeans(ApplicationContext appCtx) {
-    System.out.println("Spring IoC 컨테이너에 들어있는 객체들:");
+    logger.debug("Spring IoC 컨테이너에 들어있는 객체들:");
     String[] beanNames = appCtx.getBeanDefinitionNames();
     for (String beanName : beanNames) {
-      System.out.printf("%s =========> %s\n",
-          beanName,
-          appCtx.getBean(beanName).getClass().getName());
+      logger.debug(String.format("%s =======> %s", //
+          beanName, //
+          appCtx.getBean(beanName).getClass().getName()));
     }
+
   }
 
   private Method getRequestHandler(Class<?> type) {
@@ -77,6 +83,7 @@ public class ContextLoaderListener implements ApplicationContextListener {
         return m;
       }
     }
+
     return null;
   }
 
--- backend-project-server/src/main/java/com/eomcs/lms/DatabaseConfig.java
+++ backend-project-server/src/main/java/com/eomcs/lms/DatabaseConfig.java
@@ -1,6 +1,8 @@
 package com.eomcs.lms;
 
 import javax.sql.DataSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,17 +10,25 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-// Spring IoC 컨테이너가 이 클래스를 Java Config 로 자동 인식하려면
+// Spring IoC 컨테이너가 이 클래스를 Java Config로 자동 인식하려면
 // 다음 태그를 붙여야 한다.
 // 단, 이 클래스가 @ComponentScan 에서 지정한 패키지 안에 있어야 한다.
 @Configuration
 
-// Spring IoC 컨테이너에서 사용하라 Properties 파일을 로딩하기
+// 다음 애노테이션을 선언하면,
+// @Transactional 이 붙은 메서드가 있을 경우
+// 트랜잭션 제어 코드가 삽입된 프록시 객체를 자동생성한다.
+@EnableTransactionManagement
+
+// Spring IoC 컨테이너에서 사용할 Properties 파일을 로딩하기
 @PropertySource("classpath:com/eomcs/lms/conf/jdbc.properties")
 public class DatabaseConfig {
 
-  // @PropertySource 로 로딩하나 .properties 파일의 값을 사용하고 싶다면,
+  static Logger logger = LogManager.getLogger(DatabaseConfig.class);
+
+  // @PropertySource로 로딩한 .properties 파일의 값을 사용하고 싶다면,
   // 다음 애노테이션을 인스턴스 필드 앞에 붙여라.
   // Spring IoC 컨테이너가 이 클래스의 객체를 생성할 때
   // 해당 필드에 프로퍼티 값을 자동으로 주입할 것이다.
@@ -35,14 +45,14 @@ public class DatabaseConfig {
   String jdbcPassword;
 
   public DatabaseConfig() {
-    System.out.println("DatabaseConfig 객체 생성!!");
+    logger.debug("DatabaseConfig 객체 생성!");
   }
 
   // Spring IoC 컨테이너에 수동으로 객체를 등록하고 싶다면,
-  // 그 객체를 만들어 주 는 팩토리 메서드를 정의해야 한다.
-  // => 단, 메서드 선언부에 @Bean 애노테이션을 붙여야 한다.
+  // 그 객체를 만들어 주는 팩토리 메서드를 정의해야 한다.
+  // => 단 메서드 선언부에 @Bean 애노테이션을 붙여야 한다.
   // => 그래야만 Spring IoC 컨테이너는
-  //    이 메서드를 호출하고 그 리턴값을 보관한다.
+  // 이 메서드를 호출하고 그 리턴 값을 보관한다.
 
   @Bean
   public DataSource dataSource() {
@@ -54,7 +64,6 @@ public class DatabaseConfig {
     return ds;
   }
 
-
   @Bean
   public PlatformTransactionManager transactionManager(
       // 필요한 값이 있다면 이렇게 파라미터로 선언만 하라.
@@ -63,3 +72,5 @@ public class DatabaseConfig {
     return new DataSourceTransactionManager(dataSource);
   }
 }
+
+
